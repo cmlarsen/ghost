f2fc913e33b218f23b2ad5d1dbf327c862979e84
Caleb Larsen <cmlarsen@gmail.com>
2018-02-21 23:09:24 -0500

    get access token for user

--- modules/activityService.js
+++ modules/activityService.js
@@ -1,19 +1,28 @@
 const strava = require("strava-v3");
+
+const profile = request("./profile.js");
 const activityService = {};
 
 activityService.incoming = event => {
   if (event.object_type === "activity") {
-    activityService.getActivity(event.object_id);
+    activityService.getActivity(event);
   }
 };
 
-activityService.getActivity = id => {
-  strava.activities.get({ id }, function(err, payload, limits) {
-    if (!err) {
-      console.log("Activity:" + JSON.stringify(payload));
-    } else {
-      console.log(err);
-    }
+activityService.getActivity = event => {
+  const { owner_id, object_id } = event;
+  profile.fetchByStravaId(owner_id).then(profile => {
+    //need to get the users access token and pass it along here.
+    strava.activities.get(
+      { id: object_id, access_token: profile.access_token },
+      function(err, payload, limits) {
+        if (!err) {
+          console.log("Activity:" + JSON.stringify(payload));
+        } else {
+          console.log(err);
+        }
+      }
+    );
   });
 };
 
